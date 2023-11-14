f62d4620f5a754520869a1807e65018f2e045289
wavesinaroom <jaureguij@javeriana.edu.co>
2023-11-14 12:53:10 -0500

    [Feature] Index route

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 const port = 3000;
-
-app.get('/',(req,res)=>{
-  res.send("Hello World");
-})
 
 app.listen(port, ()=>{
   console.log(`Example app listening on port ${port}`);
 })
+
+app.get("/", function(req,res){
+  res.sendFile("./index.html", {root: __dirname});
+});
