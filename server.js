a27c4efe9bc88c4cb2fc813cd931c5ce83469fbb
wavesinaroom <jaureguij@javeriana.edu.co>
2023-11-14 15:51:11 -0500

    [Task] Route the rest of server responses

--- server.js
+++ server.js
@@ -10,3 +10,17 @@ app.listen(port, ()=>{
 app.get("/", function(req,res){
   res.sendFile("./index.html", {root: __dirname});
 });
+
+app.get("/about", function(req,res){
+  res.sendFile("./about.html", {root: __dirname});
+});
+
+app.get("/contact", function(req,res){
+  res.sendFile("./contact.html", {root: __dirname});
+});
+
+app.use(function(req,res,next){
+  res.status(404).sendFile("./404.html", {root: __dirname});
+});
+
+
